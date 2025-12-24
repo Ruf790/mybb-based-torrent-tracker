8c4c0d7e8e038109fb3629945b607885290b29bb
Ruf790 <165602471+Ruf790@users.noreply.github.com>
2025-12-24 13:27:48 +0200

    Clean up stats array by removing unused fields
    Removed unused statistics from the stats array.

--- cache/stats.php
+++ cache/stats.php
@@ -6,19 +6,7 @@
 */
 
 $stats = array (
-  'numthreads' => 0,
-  'numunapprovedthreads' => 0,
-  'numposts' => 0,
-  'numunapprovedposts' => 0,
-  'numusers' => 1,
-  'lastuid' => 132,
-  'lastusername' => 'Admins',
-  'leechers' => '0',
-  'seeders' => '0',
-  'torrents' => 0,
-  'peers' => '0',
-  'totaldownloaded' => 0,
-  'totaluploaded' => 11811160064,
+
 );
 
 ?>

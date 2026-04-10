18ed3acb6765f07f83d2cbb955c01bc28c20278e
Ruf790 <165602471+Ruf790@users.noreply.github.com>
2026-04-10 14:02:35 +0300

    Delete cache directory

--- cache/freeleech.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Cache Name: FreeLeech
- * Generated: Fri, 07 Nov 2025 11:49:05 +0000
-*/
-$__FLSTYPE = 'freeleech';
-$__F_START = '2025-09-08 12:46:00';
-$__F_END   = '2025-09-15 12:46:00';
-?>
--- cache/lastcache.php
+++
@@ -1,7 +0,0 @@
-<?php
-/** TS Generated Cache#13 - Do Not Alter
- * Cache Name: LastCache
- * Generated: Wed, 24 Sep 2008 23:43:07 +0000
-*/
-$__LastCache = '';
-?>

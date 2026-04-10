ed293192a91fbc960cee9021baf311db122bc052
Ruf790 <165602471+Ruf790@users.noreply.github.com>
2026-04-10 13:56:06 +0300

    Update SMTP.php

--- cache/SMTP.php
+++ cache/SMTP.php
@@ -1,20 +1,9 @@
 <?php
 
-/** Ruff Tracker Generated Cache - Do Not Alter
- * Cache Name: SMTP
- * Generated: Tue, 11 Mar 2025 08:21:48 +0000
-*/
+
 
 $SMTP = array (
-  'mail_handler' => 'smtp',
-  'smtp_host' => 'smtp.gmail.com',
-  'smtp_port' => '777',
-  'smtp_user' => 'a@gmail.com',
-  'smtp_pass' => 'pass',
-  'mail_logging' => '2',
-  'secure_smtp' => '2',
-  'mail_message_id' => '1',
-  'mail_queue_limit' => '10',
+ 
 );
 
 ?>

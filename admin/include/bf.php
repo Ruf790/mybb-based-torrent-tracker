fd96c271e5a4fdfcd37407eae472ab6c4f47a852
Ruf790 <165602471+Ruf790@users.noreply.github.com>
2026-04-29 15:17:26 +0300

    Delete admin/include directory

--- admin/include/bf.php
+++
@@ -1,4 +0,0 @@
-<?php
-/*Do not change following line, otherwise script wont work correctly*/
-$bf_server_name = '';
-?>
--- admin/include/remote_control.php
+++
@@ -1,4 +0,0 @@
-Content-type: text/html
-X-Powered-By: PHP/4.3.11
-
-<br>The encoded file <b>/home/lkq/ioncube/src/TS56/TS5.6/admin/include/remote_control.php</b> is corrupt.

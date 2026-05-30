0e416ae0db6baa561aaab784c18388e8921ca183
Ruf790 <165602471+Ruf790@users.noreply.github.com>
2026-05-30 14:16:47 +0300

    Add files via upload

--- ajax_imdb.php
+++ ajax_imdb.php
@@ -1,83 +1,53 @@
-<?
+<?php
+declare(strict_types=1);
 
+define('SKIP_LOCATION_SAVE', true);
+define('DEBUGMODE', false);
 
+require 'global.php';
+gzip();
 
-  function show_msg ($message = '', $error = false)
-  {
+function show_msg(string $message = '', bool $error = false): void
+{
     global $shoutboxcharset;
-    header ('Expires: Sat, 1 Jan 2000 01:00:00 GMT');
-    header ('Last-Modified: ' . gmdate ('D, d M Y H:i:s') . 'GMT');
-    header ('Cache-Control: no-cache, must-revalidate');
-    header ('Pragma: no-cache');
-    header ('' . 'Content-type: text/html; charset=' . $shoutboxcharset);
-    if ($error)
-    {
-      exit ('<error>' . $message . '</error>');
+    header('Expires: Sat, 1 Jan 2000 01:00:00 GMT');
+    header('Last-Modified: ' . gmdate('D, d M Y H:i:s') . ' GMT');
+    header('Cache-Control: no-cache, must-revalidate');
+    header('Pragma: no-cache');
+    header('Content-type: text/html; charset=' . $shoutboxcharset);
+    if ($error) {
+        exit('<error>' . $message . '</error>');
     }
+    exit($message);
+}
 
-    exit ($message);
-  }
+if (strtoupper($_SERVER['REQUEST_METHOD']) !== 'POST' || !$CURUSER || !is_mod($usergroups)) {
+    exit();
+}
 
-  define ('SKIP_LOCATION_SAVE', true);
-  define ('DEBUGMODE', false);
-  require 'global.php';
-  gzip ();
+if (!isset($_POST['tid']) || !is_valid_id($_POST['tid'])) {
+    show_msg($lang->global['notorrentid'], true);
+}
 
-  define ('TS_AJAX_VERSION', '1.2.0 ');
-  define ('TU_VERSION', '2.6.6 ');
-  if ((((!defined ('IN_SCRIPT_TSSEv56') OR strtoupper ($_SERVER['REQUEST_METHOD']) != 'POST') OR !$CURUSER) OR !is_mod ($usergroups)))
-  {
-    exit ();
-  }
+$id = (int)$_POST['tid'];
 
-  if ((!isset ($_POST['tid']) OR !is_valid_id ($_POST['tid'])))
-  {
-    show_msg ($lang->global['notorrentid'], true);
-  }
+$query  = $db->sql_query_prepared('SELECT t_link FROM torrents WHERE id = ?', [$id]);
+if ($db->num_rows($query) == 0) {
+    show_msg($lang->global['notorrentid'], true);
+}
 
-  
-  
-  $id = (int)$_POST['tid'];
+$result    = $db->fetch_array($query);
+$oldt_link = $result['t_link'] ?? '';
 
-  $sql = "SELECT t_link FROM torrents WHERE id = ?";
-  $params = [$id];
+if (!$oldt_link) {
+    show_msg($lang->global['notorrentid'], true);
+}
 
-  $Query = $db->sql_query_prepared($sql, $params);
+preg_match("@<a href='(.*)'@U", $oldt_link, $imdblink);
+$t_link = $imdblink[1] ?? '';
 
-  
-  
-  
-  if ($db->num_rows ($Query) == 0)
-  {
-    show_msg ($lang->global['notorrentid'], true);
-  }
-
-  
-  $Result = $db->fetch_array($Query);
-  $oldt_link = $Result["t_link"];
-
-  if (!$oldt_link)
-  {
-    show_msg ($lang->global['notorrentid'], true);
-  }
-
-  preg_match ('@<a href=\'(.*)\'@U', $oldt_link, $imdblink);
-  $t_link = $imdblink[1];
-  if ($t_link)
-  {
+if ($t_link) {
     include_once INC_PATH . '/ts_imdb.php';
-    if ($t_link)
-    {
-      
-	  
-	  $sql = "UPDATE torrents SET t_link = ? WHERE id = ?";
-      $params = [$t_link, (int)$id];
-
-      $db->sql_query_prepared($sql, $params);
-	  
-
-      show_msg ($t_link);
-    }
-  }
-
-?>
+    $db->sql_query_prepared('UPDATE torrents SET t_link = ? WHERE id = ?', [$t_link, $id]);
+    show_msg($t_link);
+}

9cf2e3e967c87749defab5b2ceb10ac9571c251b
Ruf790 <165602471+Ruf790@users.noreply.github.com>
2026-05-31 12:37:20 +0300

    Add files via upload

--- include/cachehandlers/disk.php
+++ include/cachehandlers/disk.php
@@ -1,126 +1,79 @@
 <?php
-/**
- * MyBB 1.8
- * Copyright 2014 MyBB Group, All Rights Reserved
- *
- * Website: http://www.mybb.com
- * License: http://www.mybb.com/about/license
- *
- */
+declare(strict_types=1);
 
-/**
- * Disk Cache Handler
- */
 class diskCacheHandler implements CacheHandlerInterface
 {
-	/**
-	 * Connect and initialize this handler.
-	 *
-	 * @return boolean True if successful, false on failure
-	 */
-	function connect()
-	{
-		if(!@is_writable(TSDIR."/cache"))
-		{
-			return false;
-		}
+    public function connect(): bool
+    {
+        return is_writable(TSDIR . '/cache');
+    }
 
-		return true;
-	}
+    public function fetch(string $name): mixed
+    {
+        $file = TSDIR . "/cache/{$name}.php";
+        if (!file_exists($file)) {
+            return false;
+        }
+        include $file;
+        return $$name ?? false;
+    }
 
-	/**
-	 * Retrieve an item from the cache.
-	 *
-	 * @param string $name The name of the cache
-	 * @return mixed Cache data if successful, false if failure
-	 */
-	function fetch($name)
-	{
-		if(!@file_exists(TSDIR."/cache/{$name}.php"))
-		{
-			return false;
-		}
+    public function put(string $name, mixed $contents): bool
+    {
+        global $mybb;
 
-		@include(TSDIR."/cache/{$name}.php");
+        $dir = TSDIR . '/cache';
+        if (!is_writable($dir)) {
+            $mybb->trigger_generic_error('cache_no_write');
+            return false;
+        }
 
-		// Return data
-		return $$name;
-	}
+        $path = "{$dir}/{$name}.php";
+        $data = "<?php\n\n/** Generated Cache - Do Not Alter\n"
+              . " * Cache Name: {$name}\n"
+              . ' * Generated: ' . gmdate('r') . "\n*/\n\n"
+              . "\${$name} = " . var_export($contents, true) . ";\n\n?>";
 
-	/**
-	 * Write an item to the cache.
-	 *
-	 * @param string $name The name of the cache
-	 * @param mixed $contents The data to write to the cache item
-	 * @return boolean True on success, false on failure
-	 */
-	function put($name, $contents)
-	{
-		global $mybb;
-		if(!is_writable(TSDIR."/cache"))
-		{
-			$mybb->trigger_generic_error("cache_no_write");
-			return false;
-		}
+        $fp = fopen($path, 'w');
+        if (!$fp) {
+            $mybb->trigger_generic_error('cache_no_write');
+            return false;
+        }
 
-		$cache_file = fopen(TSDIR."/cache/{$name}.php", "w") or $mybb->trigger_generic_error("cache_no_write");
-		flock($cache_file, LOCK_EX);
-		$cache_contents = "<?php\n\n/** Generated Cache - Do Not Alter\n * Cache Name: $name\n * Generated: ".gmdate("r")."\n*/\n\n";
-		$cache_contents .= "\$$name = ".var_export($contents, true).";\n\n?>";
-		fwrite($cache_file, $cache_contents);
-		flock($cache_file, LOCK_UN);
-		fclose($cache_file);
+        flock($fp, LOCK_EX);
+        fwrite($fp, $data);
+        flock($fp, LOCK_UN);
+        fclose($fp);
 
-		return true;
-	}
+        return true;
+    }
 
-	/**
-	 * Delete a cache
-	 *
-	 * @param string $name The name of the cache
-	 * @return boolean True on success, false on failure
-	 */
-	function delete($name)
-	{
-		return @unlink(TSDIR."/cache/{$name}.php");
-	}
+    public function delete(string $name): bool
+    {
+        $file = TSDIR . "/cache/{$name}.php";
+        return file_exists($file) && unlink($file);
+    }
 
-	/**
-	 * Disconnect from the cache
-	 *
-	 * @return bool
-	 */
-	function disconnect()
-	{
-		return true;
-	}
+    public function disconnect(): bool
+    {
+        return true;
+    }
 
-	/**
-	 * Select the size of the disk cache
-	 *
-	 * @param string $name The name of the cache
-	 * @return integer the size of the disk cache
-	 */
-	function size_of($name='')
-	{
-		if($name != '')
-		{
-			return @filesize(TSDIR."/cache/{$name}.php");
-		}
-		else
-		{
-			$total = 0;
-			$dir = opendir(TSDIR."/cache");
-			while(($file = readdir($dir)) !== false)
-			{
-				if($file == "." || $file == ".." || $file == ".svn" || !is_file(TSDIR."/cache/{$file}"))
-				{
-					continue;
-				}
+    public function size_of(string $name = ''): int
+    {
+        if ($name !== '') {
+            return (int)filesize(TSDIR . "/cache/{$name}.php");
+        }
 
-				$total += filesize(TSDIR."/cache/{$file}");
-			}
-			return $total;
-		}
-	}
+        $total = 0;
+        $dir   = TSDIR . '/cache';
+
+        foreach (new DirectoryIterator($dir) as $file) {
+            if ($file->isFile()) {
+                $total += $file->getSize();
+            }
+        }
+
+        return $total;
+    }
 }
--- include/cachehandlers/interface.php
+++ include/cachehandlers/interface.php
@@ -1,61 +1,12 @@
 <?php
-/**
- * MyBB 1.8
- * Copyright 2014 MyBB Group, All Rights Reserved
- *
- * Website: http://www.mybb.com
- * License: http://www.mybb.com/about/license
- *
- */
+declare(strict_types=1);
 
-/**
- * Cache Handler Interface
- */
 interface CacheHandlerInterface
 {
-	/**
-	 * Connect and initialize this handler.
-	 *
-	 * @return boolean True if successful, false on failure
-	 */
-	function connect();
-
-	/**
-	 * Connect and initialize this handler.
-	 *
-	 * @param string $name
-	 * @return boolean True if successful, false on failure
-	 */
-	function fetch($name);
-
-	/**
-	 * Write an item to the cache.
-	 *
-	 * @param string $name The name of the cache
-	 * @param mixed $contents The data to write to the cache item
-	 * @return boolean True on success, false on failure
-	 */
-	function put($name, $contents);
-
-	/**
-	 * Delete a cache
-	 *
-	 * @param string $name The name of the cache
-	 * @return boolean True on success, false on failure
-	 */
-	function delete($name);
-
-	/**
-	 * Disconnect from the cache
-	 *
-	 * @return bool
-	 */
-	function disconnect();
-
-	/**
-	 * @param string $name
-	 *
-	 * @return string
-	 */
-	function size_of($name='');
+    public function connect(): bool;
+    public function fetch(string $name): mixed;
+    public function put(string $name, mixed $contents): bool;
+    public function delete(string $name): bool;
+    public function disconnect(): bool;
+    public function size_of(string $name = ''): int;
 }

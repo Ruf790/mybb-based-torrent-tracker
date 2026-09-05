503371f50575b8ae4a545b0480f7569a51d8cfdb
Ruf790 <165602471+Ruf790@users.noreply.github.com>
2026-09-05 11:42:01 +0300

    Delete scripts directory

--- scripts/tooltip.js
+++
@@ -1,7 +0,0 @@
-// tooltip.js
-document.addEventListener('DOMContentLoaded', function() {
-    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-toggle="tooltip"]'));
-    var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-        return new bootstrap.Tooltip(tooltipTriggerEl);
-    });
-});
--- scripts/usercp-options.js
+++
@@ -1,6 +0,0 @@
-function toggleCatSub(btn) {
-    btn.classList.toggle("active");
-    const selected = [...document.querySelectorAll("#catSubsPicker .cat-pick-btn.active")]
-        .map(b => "[cat" + b.dataset.id + "]").join("");
-    document.getElementById("catSubsSelected").value = selected;
-}
